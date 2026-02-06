c31a9de1170569c0d07807c831fe9743abda6d63
Eduardo Duarte Cunha <edu.duartecunha@gmail.com>
2026-02-06 10:13:51 -0300

    chore: define project folder architecture

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
+import Header from "./components/layout/Header";
+import Hero from "./components/sections/Hero";
+import About from "./components/sections/About";
+import Projects from "./components/sections/Projects";
+
 function App() {
   return (
-    <div className="min-h-screen bg-neutral-950 text-neutral-100 flex items-center justify-center">
-      <h1 className="text-4xl font-semibold">
-        Tailwind is working!
-      </h1>
-    </div>
+    <>
+      <Header />
+      <Hero />
+      <About />
+      <Projects />
+    </>
   );
 }
 
